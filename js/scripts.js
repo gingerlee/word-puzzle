d592944aef8096ea1f0828549f3b31ad1f962981
Cat Janowitz and Ginger Lee Kretschmer <cat@>
2017-10-24 14:39:35 -0700

    using nathan code to help write for loops nested in each other and branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
-  $("form#puzzle").submit(event) {
+  $("form#puzzle").submit(function(event) {
     event.preventDefault();
-    
+    var vowels = ["a", "e", "i", "o", "u"];
+    var fullSentence = $("input#fullSentence").val();
+    var splitSentence = fullSentence.split("");
 
+    for (var charIndex = 0; charIndex < splitSentence.length; charIndex += 1) {
+      for (var vwlIndex = 0; vwlIndex < vowels.length; vwlIndex += 1) {
+        if (splitSentence[charIndex] === vowels[vwlIndex]) {
+        splitSentence[charIndex] = "-";
+        }
+      }
+    }
+
+    var output = splitSentence.join("");
+    alert(output);
+
+
+
+
+
+
+    // var newArray = [];
+
+
+
+
+    // var puzzle = function(index) {
+    //   for (var index = )
+    // }
 
   });
 });
